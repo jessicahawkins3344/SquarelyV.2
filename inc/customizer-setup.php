33c63a39a3dd63b1f99a0b27491aa0caf58b0ca4
Jessica Hawkins <jessicarhawkins08@outlook.com>
2016-03-03 01:48:02 -0600

    added site icon back to customizer

--- inc/customizer-setup.php
+++ inc/customizer-setup.php
@@ -11,18 +11,14 @@ function squarely_update_native_customizer_functionality( $wp_customize ) {
 
     $wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
     $wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
-    $wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
-    $wp_customize->get_section( 'header_image' )->panel         = 'header-navbar';
+
     $wp_customize->get_section( 'title_tagline' )->priority     = '9';
     $wp_customize->get_section( 'title_tagline' )->title        = __('Site branding', 'squarely');
     $wp_customize->get_section( 'title_tagline' )->panel        = 'header-navbar';    
     $wp_customize->remove_control( 'header_textcolor' );
     $wp_customize->remove_control( 'display_header_text' );
-
-    if ( $wp_customize->get_control( 'site_icon' ) ) {
-      $wp_customize->get_control( 'site_icon' )->section     = 'header-navbar';
-      $wp_customize->get_control( 'site_icon' )->priority    = '1000';
-    }
+    $wp_customize->remove_section( 'colors' );
+    $wp_customize->remove_section( 'background_image' );
 
 }
 
